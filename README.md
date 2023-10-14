7378823f26b76583820b8287395e39f03d89a5e4
ErnestMaina <you@example.com>
2023-10-14 12:30:43 +0300

    second commit

---
+++ README.md
@@ -0,0 +1,36 @@
+#### The app is a simple car purchasing website created on , 12/10/2023
+#### **By Ernest Maina Karichu**
+## Project Description
+This is a simple car purchasing website that allows users to view car listings, add new cars details,update car details ,delete  and purchase cars. The project is built using HTML, CSS, and JavaScript, and it simulates interactions with a JSON server to manage car data.
+
+
+## Setup/Installation Requirements
+    - Clone the repository on your device 
+    - Open the folder with vs code.
+    - Then run "json-server --watch database.json" to run our server.
+    - The run your index.html or  double clicking of the index.html
+    - And your application is running.
+       
+
+## Known Bugs
+-The update car modal is not as tidy as the add new car modal but it meets its request.
+
+## Technologies used
+    - JS
+    - HTML and CSS
+
+## Support and contact details
+    - email :: ernest.maina@student.moringaschool.com
+    - phone :: +254718215691
+
+### License
+*Licenced under the MIT Licence
+Copyright (c) 2023 Ernest Maina Krichu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.```
+
+
---
+++ index.js
@@ -0,0 +1,260 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carListingsContainer = document.getElementById("car-listings");
+    const addCarButton = document.getElementById("add-car-button");
+    const addCarModal = document.getElementById("add-car-modal");
+    const closeAddCarModalButton = addCarModal.querySelector(".close");
+    const addCarForm = document.getElementById("add-car-form");
+
+    let cars = [];
+
+    fetch('http://localhost:3000/cars') 
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Failed to fetch car data.');
+        }
+    })
+    .then(data => {
+        cars = data; // Updating  the cars array with the fetched data
+        renderCarListings(); // Render the car listings with the updated data
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Failed to fetch car data. Please try again.');
+    });
+
+
+        // Function to render car listings
+   function renderCarListings() {
+    carListingsContainer.innerHTML = ""; // Clear existing listings
+    cars.forEach(car => {
+        const carElement = document.createElement("div");
+        carElement.classList.add("car-listing");
+        carElement.innerHTML = `
+            <img src="${car.image}" alt="${car.name}">
+            <h2>${car.name}</h2>
+            <p>${car.Description}</p>
+            <p><strong>Price: ${car.price}</strong></p>
+            <button class="purchase-button" data-car-id="${car.id}">Purchase</button>
+            <button class="delete-button" data-car-id="${car.id}">Delete</button>
+            <button class="update-button" data-car-id="${car.id}">Update</button>
+
+        `;
+        carListingsContainer.appendChild(carElement);
+
+        // Adding a click event listener for the "Purchase" button
+        const purchaseButton = carElement.querySelector(".purchase-button");
+        purchaseButton.addEventListener("click", () => {
+            alert(`Purchase is being processed...`);
+        });
+    });
+}
+
+
+
+// Function to open the "Add Car" modal
+function openAddCarModal() {
+    addCarModal.style.display = "block";
+}
+
+// Function to close the "Add Car" modal
+function closeAddCarModal() {
+    addCarModal.style.display = "none";
+    addCarForm.reset(); 
+}
+
+
+// Function to delete a car  
+function deleteCarHttp(carId) {
+    fetch(`http://localhost:3000/cars/${carId}`, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if (response.ok) {
+            // Successful DELETE request
+            return response.json();
+        } else {
+            // Handle the error if the request fails
+            throw new Error('Failed to delete the car.');
+        }
+    })
+    .then(data => {
+        // Remove the car from the local array
+        cars = cars.filter(car => car.id !== parseInt(carId));
+        renderCarListings();
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Failed to delete the car. Please try again.');
+    });
+}
+
+// Event listener for the "Delete" button
+carListingsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        const carId = event.target.getAttribute("data-car-id");
+        deleteCarHttp(carId);
+    }
+});
+
+   
+// Function to add a new car to the database
+    function addNewCar(newCar) {
+    fetch('http://localhost:3000/cars', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newCar),
+  })
+    .then(response => {
+    if (response.ok) {
+        // Successful POST request
+        return response.json();
+    } else {
+        // Handle the error if the request fails
+        throw new Error('Failed to add a new car.');
+    }
+   })
+  .then(data => {
+    cars.push(data);
+    renderCarListings();
+    closeAddCarModal();
+   })
+  .catch(error => {
+    console.error(error);
+    alert('Failed to add a new car. Please try again.');
+  });
+  }
+
+// Function to update a car by its ID
+function updateCarHttp(carId, updatedCar) {
+    fetch(`http://localhost:3000/cars/${carId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedCar),
+    })
+    .then(response => {
+        if (response.ok) {
+            // Successful PUT request
+            return response.json();
+        } else {
+            // Handle the error if the request fails
+            throw new Error('Failed to update the car.');
+        }
+    })
+    .then(data => {
+        // Finding the index of the updated car in the local array
+        const updatedIndex = cars.findIndex(car => car.id === parseInt(carId));
+        if (updatedIndex !== -1) {
+            cars[updatedIndex] = data;
+            renderCarListings();
+        }
+        closeUpdateCarModal();
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Failed to update the car. Please try again.');
+    });
+}
+
+
+// Function to open the "Update Car" modal with pre-filled data
+function openUpdateCarModal(car) {
+    const updateCarModal = document.getElementById("update-car-modal");
+    const closeUpdateCarModalButton = document.getElementById("close-update-car-modal");
+    const updateCarForm = document.getElementById("update-car-form");
+
+    const updateCarName = document.getElementById("update-car-name");
+    const updateCarImage = document.getElementById("update-car-image");
+    const updateCarDescription = document.getElementById("update-car-Description");
+    const updateCarPrice = document.getElementById("update-car-price");
+
+    updateCarName.value = car.name;
+    updateCarImage.value = car.image;
+    updateCarDescription.value = car.Description;
+    updateCarPrice.value = car.price;
+
+    updateCarModal.style.display = "block";
+
+    // Event listener for the "Close" button in the modal
+    closeUpdateCarModalButton.addEventListener("click", closeUpdateCarModal);
+
+    // Event listener for the form submission to update the car
+    updateCarForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Gather updated input values from the form
+        const updatedName = updateCarName.value;
+        const updatedImage = updateCarImage.value;
+        const updatedDescription = updateCarDescription.value;
+        const updatedPrice = updateCarPrice.value;
+
+        // Create an updated car object
+        const updatedCar = {
+            name: updatedName,
+            image: updatedImage,
+            Description: updatedDescription,
+            price: updatedPrice,
+        };
+
+        // Close the modal
+        closeUpdateCarModal();
+
+        // Calling the function to update the car on the server
+        updateCarHttp(car.id, updatedCar);
+    });
+}
+
+// Function to close the "Update Car" modal
+function closeUpdateCarModal() {
+    const updateCarModal = document.getElementById("update-car-modal");
+    updateCarModal.style.display = "none";
+}
+
+// Event listener for the "Update" button
+carListingsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("update-button")) {
+        const carId = event.target.getAttribute("data-car-id");
+        const carToUpdate = cars.find(car => car.id === parseInt(carId));
+        openUpdateCarModal(carToUpdate);
+    }
+});
+
+
+// Event listener for the "Add New Car" button
+addCarButton.addEventListener("click", openAddCarModal);
+
+// Event listener for the "Close" button in the modal
+closeAddCarModalButton.addEventListener("click", closeAddCarModal);
+
+// Event listener for the form submission to add a new car
+addCarForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Gathering input values from the form
+    const carName = document.getElementById("car-name").value;
+    const carImage = document.getElementById("car-image").value;
+    const carDescription = document.getElementById("car-description").value;
+    const carPrice = document.getElementById("car-price").value;
+
+    // Creating  a new car object
+    const newCar = {
+        id: cars.length + 1, 
+        name: carName,
+        image: carImage,
+        Description: carDescription,
+        price: carPrice,
+    };
+
+    addNewCar(newCar);
+});
+
+// Calling  the function to initially render car listings
+renderCarListings();
+});
+
